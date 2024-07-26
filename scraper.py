23347a63d38fdc96f40a4cd4824f22c7f0438843
UK-17 <utkarsh.kushagra@outlook.com>
2024-07-26 20:01:02 +0530

    Made code OOP based

--- scraper.py
+++ scraper.py
@@ -12,168 +12,149 @@ import pandas as pd
 import requests
 from PIL import Image
 from bs4 import BeautifulSoup
-from requests.models import Response
 
 logging.config.fileConfig('logging.conf')
 logger = logging.getLogger(__name__)
 
+TIMEOUT = 10  # global timeout for request function
 
-class Metadata:
+
+class Website:
+    """
+    The main 4chan page.
+
     """
 
-    Extracting metadata about all the boards on 4chan using its API:
-    1. Display List of boards on 4chan. Select a board from the list.
-    2. Display all threads of the board.Select a thread from the list.
-
-    """
-
-    TIMEOUT = 10  # global timeout for request function
     BOARD_DATA = 'https://a.4cdn.org/boards.json'  # to get list of boards
-    CATALOG_DATA = 'https://a.4cdn.org/{board_code}/catalog.json'  # to get threads list
 
     def __init__(self) -> None:
 
         """ Initialize all the metadata. Select board code. """
 
         print('\nGathering metadata on the threads...')
-        self.boards_list = Metadata.__get_boards_list()
-        self.board_code = self.__select_board()
-        self.threads = self.__get_board_threads()
-        self.thread_id = self.__select_thread()
-    
-    @staticmethod
-    def __get_boards_list() -> dict:
-        
+        self.boards = []
+
+    def get_boards(self) -> dict:
+
         """ Get a list of image boards and codes. """
 
-        print('\nFetching all the imageboards available...')
+        print('\nFetching all the image boards available...')
         try:
-            __response = requests.get(Metadata.BOARD_DATA, timeout=Metadata.TIMEOUT)  # make request to API
+            __response = requests.get(Website.BOARD_DATA, timeout=TIMEOUT)  # make request to API
         except Exception as e:
             logger.error(e)  # error in making request
-            logger.critical(f'Error in request. | URL : {Metadata.BOARD_DATA}')
+            logger.critical(f'Error in request. | URL : {Website.BOARD_DATA}')
             return {}
 
-        raw_data = json.loads(__response.content)['boards']  # extract json from response
-        boards_list = dict()
+        __index = 0
+        __board_data = json.loads(__response.content)['boards']  # extract json from response
 
-        for index, each in enumerate(raw_data):
-            boards_list[str(index)] = {"board_name": each['title'], "board_code": each['board'],
-                                       "work_safe": each['ws_board']}  # make custom dictionary
+        for board in __board_data:
+            __index += 1
+            board['index'] = __index
+            self.boards.append(Board(board))
 
-        return boards_list
-
-    def __display_board_list(self) -> None:
+    def display_boards(self) -> None:
 
         """ Display List of boards to select from """
 
         ch = int(input(
             '\n1.NSFW Boards\n2.General Boards\n3.All Boards\nEnter your choice:'))  # take choice on NSFW/SFW board.
         print('\nSelect a board from the following:')
-        for index, details in self.boards_list.items():
-            if ch == 1 and int(details['work_safe']) == 0:
-                print(f"{index}.{details['board_name']}")
-            if ch == 2 and int(details['work_safe']) == 1:
-                print(f"{index}.{details['board_name']}")
+        for board in self.boards:
+            if ch == 1 and int(board.work_safe) == 0:
+                print(f"{board.index}.{board.name}")
+            if ch == 2 and int(board.work_safe) == 1:
+                print(f"{board.index}.{board.name}")
             if ch == 3:
-                print(f"{index}.{details['board_name']}")
+                print(f"{board.index}.{board.name}")
 
-    def __select_board(self) -> str:
 
-        """ Driver function to select board. """
+class Board:
+    """
+    Represents a board in 4chan.
 
-        self.__display_board_list()
-        selected_board_index = input('\nEnter Board Number:')
-        selected_board_code = self.boards_list[selected_board_index]['board_code']
-        return selected_board_code
+    """
 
-    def __get_board_threads(self) -> list:
+    CATALOG_DATA = 'https://a.4cdn.org/{board_code}/catalog.json'  # to get threads list
+
+    def __init__(self, b_data: dict):
+        self.index = b_data['index']
+        self.name = b_data['title']
+        self.code = b_data['board']
+        self.work_safe = b_data['ws_board']
+        self.threads = []
+
+    def get_board_threads(self):
 
         """ Get a list of threads available for the board """
 
-        print(f'Getting list of threads currently on /{self.board_code} board...')
-        threads_list = list()
-        url = self.CATALOG_DATA.format(board_code=self.board_code)  # getting threads for selected board
+        print(f'Getting list of threads currently on /{self.name} board...')
+        url = self.CATALOG_DATA.format(board_code=self.code)  # getting threads for selected board
 
         try:
-            response = requests.get(url, timeout=self.TIMEOUT)
+            response = requests.get(url, timeout=TIMEOUT)
         except Exception as e:
             logger.error(e)  # timeout error
             logger.critical(f'Error in request | URL:{url}')
-            return []
+            return
 
         raw_data = json.loads(response.content)
         for each in raw_data:
-            threads = each['threads']
-            for thread in threads:
-                thread_id = thread['no']
-                thread_info = thread['semantic_url']
-                no_of_images = int(thread['images'])
-                if no_of_images > 0:
-                    threads_list.append((thread_id, thread_info, no_of_images))  # Thread-ID|Label|Images
+            for thread in each['threads']:
+                thread['board_code'] = self.code
+                self.threads.append(Thread(thread))
 
-        return threads_list
-
-    def __display_threads_list(self) -> None:
+    def display_board_threads(self) -> None:
 
         """ Display all threads on the selected board to select from. """
-
-        display_view = pd.DataFrame(self.threads, columns=['Thread-ID', 'Label', 'Images'])
+        __threads = [{'Thread-ID': thread.id,
+                      'Label': thread.info,
+                      'Images': thread.images_count} for thread in self.threads]
+        display_view = pd.DataFrame(__threads, columns=['Thread-ID', 'Label', 'Images'])
         display_view = display_view.sort_values(by='Images')
-        print(f'\nThreads currently on /{self.board_code} board :')
+        print(f'\nThreads currently on /{self.name} board :')
         print(display_view.to_string(index=False))
 
-    def __select_thread(self) -> str:
 
-        """ Select a thread from the list of threads displayed"""
-
-        self.__display_threads_list()
-        thread_id = input('\nEnter Thread-ID : ')
-        return thread_id
-
-
-class Page:
+class Thread:
     """
-    Extract all images from a given 4chan thread.
+    Represents the webpage for a particular 4chan thread.
 
     """
 
     BASE_URL = 'https://boards.4chan.org/{board_code}/thread/'  # root URL for every thread on random board
     THREAD_METADATA = 'https://a.4cdn.org/{board_code}/thread/{thread_id}.json'  # gathering info about the thread
     SAVE_DIR = str(os.curdir)  # root folder for saving images
-    TIMEOUT = 10  # global timeout for requests made
 
-    def __init__(self, metadata: Metadata) -> None:
+    def __init__(self, t_data: dict) -> None:
 
-        """ Inititalizing class and getting the page information. """
+        """ Initializing class and getting the page information. """
 
-        board_code, thread = metadata.board_code, metadata.thread_id
-        logger.info(f'Board Code:{board_code}|Thread:{thread}')
-        self.BASE_URL = self.BASE_URL.format(board_code=board_code)
+        self.id = t_data['no']
+        self.info = t_data['semantic_url']
+        self.images_count = int(t_data['images'])
+        self.images = None
+        self.url = self.BASE_URL.format(board_code=t_data['board_code']) + str(self.id)  # url of the thread
+        self.save_path = None
+        self.page = None
+        self.soup = None
+        self.is_completed = False
+        self.corrupt_files = None
 
-        self.thread_id = thread  # thread-ID from the board
-        self.save_path = self.__make_dir()  # making a directory to save all images
-        self.url = self.BASE_URL + thread  # url of the thread
-        self.page = self.__get_page()  # fetching the page
-        self.soup = self.__make_soup()  # parsing the page
-        self.images = self.__get_images('img')  # collection of all images in the page
-        self.is_completed = self.__extract_images()  # running the driver function
-
-        logger.info(f'Execution Successful : {self.is_completed}')  # if True then all execution was error free.
-
-    def __make_dir(self) -> str:
+    def make_dir(self) -> None:
 
         """ Name of the directory is same as the thread. """
 
         print(f'\nMaking a new directory to save images...')
-        path = os.path.join(self.SAVE_DIR, self.thread_id)
+        path = os.path.join(self.SAVE_DIR, str(self.id))
         os.makedirs(path)  # new directory made
         logger.info(f'Image save directory: {path}')
         print(f'New directory : {str(path)}')
 
-        return str(path)
+        self.save_path = str(path)
 
-    def __get_page(self) -> Response:
+    def get_page(self) -> None:
 
         """ Fetching HTML page using Requests library. """
 
@@ -181,15 +162,15 @@ class Page:
 
         print('\nGetting contents of the thread page...')
         try:
-            page = requests.get(self.url, timeout=Page.TIMEOUT)  # page fetched
+            page = requests.get(self.url, timeout=TIMEOUT)  # page fetched
         except Exception as e:
             logger.error(e)  # timeout error
-            logger.critical(f'URL:{self.url} | Timeout {Page.TIMEOUT}s reached')
+            logger.critical(f'URL:{self.url} | Timeout {TIMEOUT}s reached')
             page = None
 
-        return page
+        self.page = page
 
-    def __make_soup(self) -> BeautifulSoup:
+    def make_soup(self) -> None:
 
         """ Parsing the page using BeautifulSoup and HTML5LIB. """
 
@@ -197,9 +178,9 @@ class Page:
         logger.info(f'Parsing HTML content | BeautifulSoup+html5lib')
         soup = BeautifulSoup(self.page.content, 'html5lib')  # making a soup-object
 
-        return soup
+        self.soup = soup
 
-    def __get_images(self, tag: str) -> list:
+    def get_images(self, tag: str) -> None:
 
         """ Extract all the links in the page with a given tag. """
 
@@ -227,27 +208,27 @@ class Page:
         logger.info(f'No of images : {len(images)}')
         print(f'A total of {str(len(images))} image links found on the page.')
 
-        return images
+        self.images = images
 
     @staticmethod
-    def __get_image_file(image_link) -> bytes:
+    def get_image_file(image_link) -> bytes:
 
         """ Fetching an image in raw bytes. """
 
         logger.info(f'Fetching image | URL:{image_link}')
 
         try:
-            image_file = requests.get(image_link, timeout=Page.TIMEOUT)  # get request for the image
+            image_file = requests.get(image_link, timeout=TIMEOUT)  # get request for the image
         except Exception as e:
             logger.error(e)  # timeout error
-            logger.critical(f'Timeout of {Page.TIMEOUT}s|URL:{image_link}')
+            logger.critical(f'Timeout of {TIMEOUT}s|URL:{image_link}')
             return bytearray()
         image_bytes = image_file.content  # storing as bytes
 
         return image_bytes
 
     @staticmethod
-    def __save_image_file(image_bytes, image_path) -> str:
+    def save_image_file(image_bytes, image_path) -> str:
 
         """ Save each image to the directory. """
 
@@ -263,7 +244,7 @@ class Page:
 
         return f'File : {image_path}'
 
-    def __get_and_save_all_images(self) -> None:
+    def get_and_save_all_images(self) -> None:
 
         """ Driver function to fetch and save each image in the list. """
 
@@ -272,14 +253,14 @@ class Page:
             extension = 'jpg' if '.jpg' in image else 'png'
             count += 1
             print(f'Processing Image #{count}')
-            image_bytes = self.__get_image_file(image)  # fetch the image
+            image_bytes = self.get_image_file(image)  # fetch the image
             if image_bytes is None:
                 continue
             image_path = self.save_path + f'/image_{count}.{extension}'  # path to save image
-            msg = self.__save_image_file(image_bytes, image_path)  # save image to path
+            msg = self.save_image_file(image_bytes, image_path)  # save image to path
             logger.info(f'Image saved as : {msg}')
 
-    def __check_for_corrupt_files(self) -> None:
+    def check_for_corrupt_files(self) -> None:
 
         """ Verify the images and catch the corrupt ones."""
 
@@ -301,7 +282,7 @@ class Page:
 
         logger.critical(f'List of corrupt files:{self.corrupt_files}')
 
-    def __delete_corrupt_files(self) -> None:
+    def delete_corrupt_files(self) -> None:
 
         """ Delete the files which were categorized as corrupt. """
 
@@ -317,8 +298,8 @@ class Page:
 
         logger.info(f'Removed {len(self.corrupt_files)} files.')
 
-    def __get_all_file_paths(self):
-        """ Recurisvely getting all file paths of the save directory. """
+    def get_all_file_paths(self):
+        """ Recursively getting all file paths of the save directory. """
 
         # initializing empty file paths list
         file_paths = []
@@ -334,7 +315,7 @@ class Page:
         # returning all file paths
         return file_paths
 
-    def __remove_dir(self):
+    def remove_dir(self):
 
         """ Removing temp folder and files. """
 
@@ -344,30 +325,30 @@ class Page:
             logger.error(e)
             logger.critical(f'Could not remove {self.save_path}')
 
-    def __archive_dir(self) -> None:
+    def archive_dir(self) -> None:
 
         """ Archiving the thread images directory. """
 
         home_path = Path.home()
         logger.info(f'Archiving images to {home_path}')
-        files_to_archieve = self.__get_all_file_paths()
-        with ZipFile(f'{self.thread_id}.zip', 'w') as zipper:
+        files_to_archive = self.get_all_file_paths()
+        with ZipFile(f'{self.id}_{self.info}.zip', 'w') as zipper:
             # writing each file one by one
-            for file in files_to_archieve:
+            for file in files_to_archive:
                 zipper.write(file)
 
-        shutil.move(f'{self.thread_id}.zip', home_path)
-        self.__remove_dir()
+        shutil.move(f'{self.id}_{self.info}.zip', home_path)
+        self.remove_dir()
 
-    def __extract_images(self) -> bool:
+    def extract_images(self) -> None:
 
         """ Driver function to extract the images and save them. """
 
         try:
-            self.__get_and_save_all_images()
-            self.__check_for_corrupt_files()
-            self.__delete_corrupt_files()
-            self.__archive_dir()
+            self.get_and_save_all_images()
+            self.check_for_corrupt_files()
+            self.delete_corrupt_files()
+            self.archive_dir()
             flag = True
 
         except Exception as e:
@@ -375,7 +356,7 @@ class Page:
             logger.critical(f'Error in the driver function to extract images.')
             flag = False
 
-        return flag
+        self.is_completed = flag
 
 
 def exec_main() -> None:
@@ -383,7 +364,7 @@ def exec_main() -> None:
 
     # welcome message
     print(f''' *** Welcome to 4chan-scraper.***
-                   Author : Utkarsh-kushagra (UK!7)
+                   Author : (UK!7)
             
             Step 1 : Choose a board of your interest
             Step 2 : Choose a thread from that board
@@ -402,10 +383,27 @@ def exec_main() -> None:
             break  # stopping execution
 
         logger.info(f'Execution started.')
-        metadata = Metadata()
-        page = Page(metadata)
-        logger.info('Execution completed.')
-        print(f'All your downloaded files are available here : {page.save_path}')
+
+        website = Website()
+        website.get_boards()
+        website.display_boards()
+
+        board_ch = int(input('\nEnter Board Number:'))
+        for board in website.boards:
+            if board.index == board_ch:
+                print(board.index)
+                board.get_board_threads()
+                board.display_board_threads()
+                thread_id = int(input('\nEnter Thread-ID : '))
+                for thread in board.threads:
+                    if thread_id == thread.id:
+                        thread.make_dir()
+                        thread.get_page()
+                        thread.make_soup()
+                        thread.get_images('img')
+                        thread.extract_images()
+                        logger.info(f'Execution successful: {thread.is_completed}.')
+                        print(f'All your downloaded files are available here : ')
 
     # closing message 
     print('''
